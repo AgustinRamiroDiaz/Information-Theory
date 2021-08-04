6f5cdc5c04264508c335c4240bf5404ae20ff15a
Agustin Diaz <agustin.ramiro.diaz@gmail.com>
2021-08-03 21:58:19 -0300

    Added metrics

--- huffman.py
+++ huffman.py
@@ -1,8 +1,10 @@
-#%%
+# %%
 import heapq
 from dataclasses import dataclass
+from math import log
+# %%
 
-#%%
+
 @dataclass
 class Node:
     value: type
@@ -39,8 +41,27 @@ class Node:
 
         return result_dictionary
 
+    def average_length_per_symbol(self):
+        """
+        It equals the sum of all nodes in the tree minus 1,
+        therefore one can sum up all but the leaves (or sum up all but the root)
+        """
+        nodes_stack = [self]
+        result = 0
+        while nodes_stack:
+            node = nodes_stack.pop(-1)
+            if node.value == None:
+                result += node.frequency
 
-#%%
+                if node.left_node:
+                    nodes_stack.append(node.left_node)
+                if node.right_node:
+                    nodes_stack.append(node.right_node)
+
+        return result
+
+
+# %%
 def huffman_tree(value_frequency_dictionary: dict):
     priority_queue = [Node(value, frequency)
                       for value, frequency in value_frequency_dictionary.items()]
@@ -53,18 +74,44 @@ def huffman_tree(value_frequency_dictionary: dict):
 
     return priority_queue[0]
 
-#%%
+
+# %%
 message = 'una frase para explorar la compresion optima'
 
-character_frequency = {character: message.count(
-    character) for character in set(message)}
+character_probability = {character: message.count(
+    character)/len(message) for character in set(message)}
 
-tree = huffman_tree(character_frequency)
+probability_distribution = [message.count(
+    character)/len(message) for character in set(message)]
+
+# %%
+
+
+def calculate_entropy(probability_distribution):
+    probability_times_information = [probability * log(probability, 2)
+                                     for probability in probability_distribution]
+
+    entropy = -sum(probability_times_information)
+    return entropy
+
+# %%
+
+
+tree = huffman_tree(character_probability)
+
 
 encode_dictionary = tree.to_dictionary()
-decode_dictionary = {value:key for key, value in encode_dictionary.items()}
+decode_dictionary = {value: key for key, value in encode_dictionary.items()}
 
-#%%
+average_length_per_symbol = sum([len(encode_dictionary[character]) * probability for character, probability in character_probability.items()])
+entropy = calculate_entropy(probability_distribution)
+assert(entropy <= average_length_per_symbol)    # But should be close
+
+assert(abs(average_length_per_symbol - tree.average_length_per_symbol()) < 0.001)
+
+# %%
+
+
 def en_d_ecode(message, dictionary):
     result = []
     temporary = ''
@@ -73,10 +120,11 @@ def en_d_ecode(message, dictionary):
         if temporary in dictionary:
             result.append(dictionary[temporary])
             temporary = ''
-    
+
     return ''.join(result)
 
-#%%
+
+# %%
 encoded_message = en_d_ecode(message, encode_dictionary)
 decoded_message = en_d_ecode(encoded_message, decode_dictionary)
 
@@ -84,4 +132,3 @@ assert(message == decoded_message)
 print(decoded_message)
 print(encoded_message)
 
-# TODO: add metrics like entropy to test implementation
